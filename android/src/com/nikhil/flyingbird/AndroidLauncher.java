a6cb8c3b4e40d62b32e548e71f1f4a7df2bd620c
konduru nikhil <kondurunikhil07@gmail.com>
2017-07-01 14:13:43 +0530

    Flyingbird is a game made with libgdx .

---
+++ android/src/com/nikhil/flyingbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nikhil.flyingbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nikhil.flyingbird.bird.Flyingbird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Flyingbird(), config);
+	}
+}
